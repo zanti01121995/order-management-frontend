801dedd36537a93d140629c37afb8c8274e8b2ed
Zanti01121995 <santhoshsekar788@gmail.com>
2023-12-12 18:55:12 +0530

    final

--- src/components/foodDetails/foodDetails.jsx
+++ src/components/foodDetails/foodDetails.jsx
@@ -1,4 +1,4 @@
-import classes from "./foodDetails.module.css";
+import classes from "./fooddetails.module.css";
 import { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useNavigate, useParams } from "react-router-dom";
